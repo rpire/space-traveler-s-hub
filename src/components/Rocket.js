cf605f9a3dbba491242367b0b8fb8a3c2c7b0886
rpire <rubenpire7@gmail.com>
2021-10-05 16:22:10 -0400

    Use destructuring and prop validation in Rocket.js

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,16 +1,26 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const Rocket = (props) => (
+const Rocket = (props) => {
+  const {
+    rocket: {
+      rocket_name: name,
+      description,
+      flickr_images: images,
+    },
+  } = props;
+
+  return (
     <li>
       <div className="card mb-3">
         <div className="row g-0">
           <div className="col-md-4">
-            <img src={props.rocket.flickr_images[0]} className="img-fluid rounded-start" alt="..." />
+            <img src={images[0]} className="img-fluid rounded-start" alt="..." />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h3 className="card-title">{props.rocket.rocket_name}</h3>
-              <p className="card-text">{props.rocket.description}</p>
+              <h3 className="card-title">{name}</h3>
+              <p className="card-text">{description}</p>
               <button type="button" className="btn btn-primary">Reserve Rocket</button>
             </div>
           </div>
@@ -18,6 +28,12 @@ const Rocket = (props) => (
       </div>
     </li>
   );
+};
 
+Rocket.propTypes = {
+  name: PropTypes.string.isRequired,
+  describe: PropTypes.string.isRequired,
+  images: PropTypes.instanceOf(Array).isRequired,
+};
 
 export default Rocket;
