44c33ef60f9aa768871e0fd8e95b8868a5488e0d
unknown <danielmainawaweru@gmail.com>
2021-10-05 17:45:07 +0300

    Add table and install bootstrap

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -4,22 +4,42 @@ import { getMissions } from '../redux/missions/missions';
 
 const Missions = () => {
   const state = useSelector((state) => state.missions);
-  console.log(state);
   const dispatch = useDispatch();
   useEffect(() => dispatch(getMissions()), [dispatch]);
   return (
-    <div className="missions-container">
+    <div className="container">
       <h1>Missions</h1>
       <p>
         This is the space of the missions page.
       </p>
-      <ul>
+      <table className="table table-bordered border-secondary table-stripped p-3">
+        <thead>
+          <tr>
+            <th scope="col">Mission</th>
+            <th scope="col">Description</th>
+            <th scope="col">Status</th>
+            <th scope="col" className="visually-hidden">.</th>
+          </tr>
+        </thead>
         {state.map((mission) => (
-          <li key={mission.mission_id}>
-            {mission.mission_name}
-          </li>
+          <tbody key={mission.mission_id}>
+            <tr>
+              <td className="">
+                {mission.mission_name}
+              </td>
+              <td className="w-50">
+                <small>{mission.description}</small>
+              </td>
+              <td className="w-auto ">
+                <small className="border border-secondary positioning">Not a Member</small>
+              </td>
+              <td className="w-auto">
+                <button type="button" className="btn btn-secondary btn-small"> button</button>
+              </td>
+            </tr>
+          </tbody>
         ))}
-      </ul>
+      </table>
     </div>
   );
 };
