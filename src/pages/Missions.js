a84228118c7b2a690c561f669ad8a18251cdde65
unknown <danielmainawaweru@gmail.com>
2021-10-05 13:43:32 +0300

    Display missions in Mission component

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMissions } from '../redux/missions/missions';
 
 const Missions = () => {
   const state = useSelector((state) => state.missions);
+  console.log(state);
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(getMissions()), [dispatch]);
   return (
     <div className="missions-container">
       <h1>Missions</h1>
       <p>
-        {state}
-      </p>
-      <p>
         This is the space of the missions page.
       </p>
+      <ul>
+        {state.map((mission) => (
+          <li key={mission.mission_id}>
+            {mission.mission_name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
