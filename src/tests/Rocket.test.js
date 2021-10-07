d382b07b313e56c85036b56e34f6ea213eb386ea
rpire <rubenpire7@gmail.com>
2021-10-07 10:10:44 -0400

    Create tests for the Rocket component

---
+++ src/tests/Rocket.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import store from '../../src/redux/configureStore'
+import { Provider } from 'react-redux';
+import Rocket from '../components/Rocket';
+
+test('Renders Correctly', () => {
+  const rocket = {
+    rocket_name: 'One Rocket',
+    rocket_id: 'abc',
+    description: 'This is a rocket',
+    flickr_images: '#',
+    reserved: false,
+  };
+
+  render(<Provider store={store}>
+    <Rocket rocket={rocket} />
+  </Provider>);
+  expect(screen).toMatchSnapshot();
+});
