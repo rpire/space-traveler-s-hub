3cb265ca8630586646f9796e1b7ea8434c705df7
unknown <danielmainawaweru@gmail.com>
2021-10-07 11:21:56 +0300

    Import state

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div className="profile-container">
+const MyProfile = () => {
+  // const state = useSelector((state) => state.missions);
+   return (
+    <div className="profile-container">
     <h1>My Profile</h1>
     <p>
       This is the space of my profile page.
@@ -11,6 +14,7 @@ const MyProfile = () => (
       <div className="col">My Rockets</div>
     </div>
   </div>
-);
+   ) 
+   };
 
 export default MyProfile;
