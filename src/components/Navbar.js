11ea3893820f4e1fb6d06979189ae7bf3e1ae68d
rpire <rubenpire7@gmail.com>
2021-10-07 07:17:50 -0400

    Add styling for the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../media/planet.png';
 
 const links = [
   {
@@ -20,10 +21,21 @@ const links = [
 ];
 
 const Navbar = () => (
-  <nav>
-    <ul className="d-flex ps-0">
+  <nav
+    className="mx-4 d-flex justify-content-between align-items-center
+      py-3 mb-3 border-bottom border-secondary"
+  >
+    <div className="d-flex align-items-center">
+      <img
+        src={logo}
+        alt="STH Logo"
+        className="logo me-2"
+      />
+      <h1 className="mb-0">Space Traveler's Hub</h1>
+    </div>
+    <ul className="d-flex ps-0 mb-0 nav-list">
       {links.map((link) => (
-        <li key={link.id} className="mx-4">
+        <li key={link.id} className="text-primary">
           <NavLink to={link.path} activeClassName="displaying" exact>
             {link.text}
           </NavLink>
